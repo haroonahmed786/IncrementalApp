2d7d6db16909221b33a998162b6807d8ba95e95a
haroonahmed786 <Haroon65ahmed@gmail.com>
2022-09-24 05:39:01 -0700

    render method

---
+++ rendermethod/src/App.js
@@ -0,0 +1,20 @@
+import React,{Component} from "react";
+
+class App extends Component{
+    render(){
+        return (
+                <>
+        <h1>Hello RenderMethod</h1>
+        <h2>Hello world</h2>
+        <p>The term “render prop” refers to a technique for sharing code between React components 
+           using a prop whose value is a function. A component with a render ...This page contains.
+           A detailed API reference for the React component class definition. It assumes you're familiar
+           with fundamental React concepts, ...</p>
+
+    </>
+    );
+    }
+
+}
+
+export default App;
